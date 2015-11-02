e212f5e00641bda7951f70d6d426e1ba446b87d6
Lathian <Lathian@192.168.1.5>
2015-11-02 17:27:07 -0500

    Subject sequence added, lots of duplicate code need to rewrite it

--- BlAST_GUI/src/Blastn.py
+++ BlAST_GUI/src/Blastn.py
@@ -61,34 +61,20 @@ class Blastn:
         """Handles save file button putting selected file into -out"""
         pass
     def buildBlock2(self):
-        '''put in some fake data'''
-        for row in range(100):
-            tk.Label(self.inFrm, text="%s" % row, width=3, borderwidth="1", 
-                     relief="solid").grid(row=self.ROW, column=0)
-            t="this is the second column for row %s" %self.ROW
-            tk.Label(self.inFrm, text=t).grid(row=self.ROW, column=1)
-            self.ROW+=1
-    def align2OrMore(self):
-        """Destroys existing layout and then calls buildBlock method to either put in or take out extra box for
-        aligning two or more sequences locally"""
-        self.destroyInnerF()
-        self.buildInnerF()
-    #Widget Layout
-    def buildBlock1(self):        
+        #This is a lot of duplicate code perhaps a method could construct it.
         self.makeVSpace(self.inFrm)
-        #Left side padding
-        tk.Label(self.inFrm, text = '     ').grid(row = self.ROW, column = 0, rowspan = 1000)
+        #Left side padding (calculated rowspan by printing out self.ROW at end of this block)
+        tk.Label(self.inFrm, text = '     ').grid(row = self.ROW, column = 0, rowspan = 15)
         
-        tk.Label(self.inFrm, text='Enter Query Sequence:', font=('Arial', '14', 'underline')).grid(row = self.ROW , column = 1, 
+        tk.Label(self.inFrm, text='Enter Subject Sequence:', font=('Arial', '14', 'underline')).grid(row = self.ROW , column = 1, 
                                                                                                     columnspan=4, sticky = 'w')
         self.ROW += 1
         tk.Label(self.inFrm, text='Enter accession number(s), gi(s), or FASTA sequence(s)', 
                  font=('Arial', '12', 'bold')).grid(row = self.ROW , column = 1, columnspan=4, sticky ='w')
         clear_button = tk.Button(self.inFrm, text='Clear', font=('Arial', '9', 'underline'))
         clear_button.grid(row = self.ROW, column =4)
-        self.makeHSpace(self.inFrm, self.ROW, c = 5, width = 5)
-        tk.Label(self.inFrm, text='Query subrange', font=('Arial', '12', 'bold', 'underline')
-                 ).grid(row = self.ROW, column = 6, columnspan = 4)
+        tk.Label(self.inFrm, text='Subject subrange', font=('Arial', '12', 'bold', 'underline')
+                 ).grid(row = self.ROW, column = 5, columnspan = 4, sticky = 'w')
         self.ROW += 1
         # textvariable needs to be assigned to global and clear button linked to it.
         # Also it needs to be scrollable in case the user puts a lot into it
@@ -103,13 +89,54 @@ class Blastn:
         query_to = tk.Entry(self.inFrm, font=('Arial', 10), width = 8)
         query_to.grid(row = self.ROW, column = 9)
         self.ROW+=4
-        tk.Label(self.inFrm, text ='Or, upload file', font=('Arial', 12, 'bold')).grid(row = self.ROW, column=1)
+        tk.Label(self.inFrm, text ='Or, upload file', font=('Arial', 12, 'bold')).grid(row = self.ROW, column=1, sticky = 'E')
         load_query_button = tk.Button(self.inFrm, text='Choose File', command = (lambda : self.loadHandler()))
         load_query_button.grid(row = self.ROW, column = 2)
         load_status = tk.Label(self.inFrm, text='No file chosen', font=('Arial', '10'))
         load_status.grid(row = self.ROW , column = 3)
         self.ROW+=1
-        tk.Label(self.inFrm, text ='Pick save file and format', font=('Arial', 12, 'bold')).grid(row = self.ROW, column=1)
+    def align2OrMore(self):
+        """Destroys existing layout and then calls buildBlock method to either put in or take out extra box for
+        aligning two or more sequences locally"""
+        self.destroyInnerF()
+        self.buildInnerF()
+    #Widget Layout
+    def buildBlock1(self):        
+        self.makeVSpace(self.inFrm)
+        #Left side padding (calculated rowspan by printing out self.ROW at end of this block)
+        tk.Label(self.inFrm, text = '     ').grid(row = self.ROW, column = 0, rowspan = 15)
+        
+        tk.Label(self.inFrm, text='Enter Query Sequence:', font=('Arial', '14', 'underline')).grid(row = self.ROW , column = 1, 
+                                                                                                    columnspan=4, sticky = 'w')
+        self.ROW += 1
+        tk.Label(self.inFrm, text='Enter accession number(s), gi(s), or FASTA sequence(s)', 
+                 font=('Arial', '12', 'bold')).grid(row = self.ROW , column = 1, columnspan=4, sticky ='w')
+        clear_button = tk.Button(self.inFrm, text='Clear', font=('Arial', '9', 'underline'))
+        clear_button.grid(row = self.ROW, column =4)
+        tk.Label(self.inFrm, text='Query subrange', font=('Arial', '12', 'bold', 'underline')
+                 ).grid(row = self.ROW, column = 5, columnspan = 4, sticky = 'w')
+        self.ROW += 1
+        # textvariable needs to be assigned to global and clear button linked to it.
+        # Also it needs to be scrollable in case the user puts a lot into it
+        query_box = tk.Text(self.inFrm, font=('Arial', 10), width = 74, height = 5, highlightbackground = 'black', 
+                            highlightcolor = 'yellow')
+        query_box.grid(row = self.ROW, column = 1, columnspan = 6, rowspan = 5, sticky = 'w')
+        tk.Label(self.inFrm, text = 'From').grid(row = self.ROW, column = 8)
+        query_from = tk.Entry(self.inFrm, font=('Arial', 10), width = 8)
+        query_from.grid(row = self.ROW, column = 9)
+        self.ROW+=1
+        tk.Label(self.inFrm, text = 'To').grid(row = self.ROW, column = 8)
+        query_to = tk.Entry(self.inFrm, font=('Arial', 10), width = 8)
+        query_to.grid(row = self.ROW, column = 9)
+        self.ROW+=4
+        tk.Label(self.inFrm, text ='Or, upload file', font=('Arial', 12, 'bold')).grid(row = self.ROW, column=1, sticky = 'E')
+        load_query_button = tk.Button(self.inFrm, text='Choose File', command = (lambda : self.loadHandler()))
+        load_query_button.grid(row = self.ROW, column = 2)
+        load_status = tk.Label(self.inFrm, text='No file chosen', font=('Arial', '10'))
+        load_status.grid(row = self.ROW , column = 3)
+        self.ROW+=1
+        tk.Label(self.inFrm, text ='Pick save file and format', font=('Arial', 12, 'bold')).grid(row = self.ROW, 
+                                                                                                 column=1, sticky = 'E')
         save_query_button = tk.Button(self.inFrm, text='Choose File', command = (lambda : self.saveHandler()))
         save_query_button.grid(row = self.ROW, column = 2)
         save_status = tk.Label(self.inFrm, text='No file chosen', font=('Arial', '10'))
@@ -118,7 +145,7 @@ class Blastn:
         save_output_button = tk.Button(self.inFrm, text='Choose Output')
         save_output_button.grid(row = self.ROW, column = 4)
         self.ROW+=1
-        tk.Label(self.inFrm, text ='Job Title', font=('Arial', 12, 'bold')).grid(row = self.ROW, column=1)
+        tk.Label(self.inFrm, text ='Job Title', font=('Arial', 12, 'bold')).grid(row = self.ROW, column=1, sticky = 'E')
         job_title = tk.Entry(self.inFrm, font=('Arial', 10))
         job_title.grid(row = self.ROW, column = 2, columnspan = 8)
         job_title.configure(width=80)
